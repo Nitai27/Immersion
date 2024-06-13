f13d521acfa6ce7d4f8577194e4a5397392a2a6d
Nitai27 <nitaigupta27@gmail.com>
2024-06-13 22:55:07 +0530

    Add files via upload

---
+++ src/Java code/Hello.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+      StringBuffer b = new StringBuffer("hello");
+      b.reverse();
+      
+      System.out.println(b);
+    }
+}
---
+++ src/Java code/Matrix.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/**
+ * Matrix
+ */
+public class Matrix {
+
+    public static void main(String[] args) {
+        int a[][] = {
+            {6,2,1,3},
+            {4,2,1,5},
+            {9,2,8,7},
+            {4,1,2,9},
+        };
+        int k = a.length;
+        int sum=0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                
+            }
+        }
+        System.out.println(sum);
+        
+    }
+}
